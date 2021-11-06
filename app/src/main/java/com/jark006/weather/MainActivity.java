54560016d9f553aaafcbc082cec666239e790ec2
jark006 <jark006@qq.com>
2021-11-07 03:31:30 +0800

    去除启动activity，精简代码

--- app/src/main/java/com/jark006/weather/MainActivity.java
+++ app/src/main/java/com/jark006/weather/MainActivity.java
@@ -1,113 +1,20 @@
 package com.jark006.weather;
 
-import static android.Manifest.permission.ACCESS_BACKGROUND_LOCATION;
-import static android.content.ContentValues.TAG;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.util.List;
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity {
     //https://api.caiyunapp.com/v2/TwsDo9aQUYewFhV8/113.27324,23.15792/weather.json?dailysteps=6
 
-    //    private LocationManager locationManager;
-    private FusedLocationProviderClient fusedLocationClient;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        TextView mainText = findViewById(R.id.mainText);
+//        mainText.setText("OK");
+//    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-
-        TextView mainText = findViewById(R.id.mainText);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            Log.w(TAG, "onCreate: " + location.getLongitude(), null);
-                            mainText.setText("Longitude " + location.getLongitude());
-                        }else{
-                            mainText.setText("EEE ");
-
-                        }
-                    }
-                });
-
-//        Location ll = getLocation(getApplicationContext());
-
-//        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-//
-//        Location ll = getLastKnownLocation(locationManager);
-//
-//        if (ll == null) {
-//
-//
-//            Log.w(TAG, "onCreate: Location ERROR", null);
-//            mainText.setText("FAIL ll == null");
-//        } else {
-//            Log.w(TAG, "onCreate: " + ll.getLongitude(), null);
-//            mainText.setText("Longitude " + ll.getLongitude());
-//
-//        }
-    }
-
-
-    private Location getLastKnownLocation(LocationManager locationManager) {
-        List<String> providers = locationManager.getProviders(true);
-        Location bestLocation = null;
-        for (String provider : providers) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return null;
-            }
-            Location l = locationManager.getLastKnownLocation(provider);
-            if (l == null) {
-                continue;
-            }
-            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
-                // Found best last known location: %s", l);
-                bestLocation = l;
-            }
-        }
-        return bestLocation;
-    }
 
 }
--- app/src/main/java/com/jark006/weather/Widget1.java
+++ app/src/main/java/com/jark006/weather/Widget1.java
@@ -1,124 +1,42 @@
 package com.jark006.weather;
 
-import static android.content.ContentValues.TAG;
-
-import android.Manifest;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.jark006.weather.bean.Daily;
 import com.jark006.weather.bean.Realtime;
 import com.jark006.weather.bean.Skycon;
 import com.jark006.weather.bean.Temperature;
 import com.jark006.weather.bean.WeatherBean;
-
 import java.util.Date;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-
 import retrofit2.Response;
-
 import com.jark006.weather.bean.Result;
 import com.jark006.weather.utils.DateUtils;
 import com.jark006.weather.utils.ImageUtils;
-//import com.jark006.weather.utils.TranslationUtils;
 /**
  * Implementation of App Widget functionality.
  */
 public class Widget1 extends AppWidgetProvider {
-//    private int[] temperature = new int[3];
-//    private String location = "unknow", discript = "unkown";
-//    private boolean isSuccess = false;
-
-//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-//                                int appWidgetId) {
-//
-////        new Thread(networkTask).start();
-//
-//        CharSequence widgetText = context.getString(R.string.appwidget_text);
-//        // Construct the RemoteViews object
-//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget1);
-//        views.setTextViewText(R.id.discription, widgetText);
-//
-//        // Instruct the widget manager to update the widget
-//        appWidgetManager.updateAppWidget(appWidgetId, views);
-//    }
-//
-//    @Override
-//    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-//        // There may be multiple widgets active, so update all of them
-//        for (int appWidgetId : appWidgetIds) {
-//            updateAppWidget(context, appWidgetManager, appWidgetId);
-//        }
-//    }
-//
-//    @Override
-//    public void onEnabled(Context context) {
-//        // Enter relevant functionality for when the first widget is created
-//    }
-//
-//    @Override
-//    public void onDisabled(Context context) {
-//        // Enter relevant functionality for when the last widget is disabled
-//    }
-//
-//    private void parseDate(String str){
-//
-//    }
-//    private final Handler handler = new Handler(Looper.getMainLooper()) {
-//        @Override
-//        public void handleMessage(Message msg) {
-//            super.handleMessage(msg);
-//            Bundle b = msg.getData();
-//            String rawStr = b.getString("rawDate");
-//            if(rawStr == null || rawStr.length() < 10){
-////                Toast.makeText(, )
-//                Log.e(TAG, "handleMessage: 网络错误", null);
-//            }else
-//                parseDate(rawStr);
-//        }
-//    };
-//
-//    Runnable networkTask = () -> {
-////        String link = "https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=04&provinceId=0&isVerify=1&termLimits=100";
-//        String link = "https://api.caiyunapp.com/v2/TwsDo9aQUYewFhV8/113.27324,23.15792/weather.json?dailysteps=6";
-//        String Response = utils.getData(link);
-//        Message msg = new Message();
-//        Bundle data = new Bundle();
-//        Log.i(TAG, "ResponseLength: "+Response);
-//        data.putString("rawDate", Response);
-//        msg.setData(data);
-//        handler.sendMessage(msg);
-//    };
-
-
-
 
     /**
      * 更新
      */
-    public static final String ACTION_UPDATE = "action_update";
-    /**
-     * 无定位
-     */
-    public final int NO_LOCATION = 0x01;
-    /**
-     * 正在更新
-     */
-    public final int UPDATING = 0x02;
+//    public static final String ACTION_UPDATE = "action_update";
+//    /**
+//     * 无定位
+//     */
+//    public final int NO_LOCATION = 0x01;
+//    /**
+//     * 正在更新
+//     */
+//    public final int UPDATING = 0x02;
     /**
      * 更新成功
      */
@@ -169,47 +87,16 @@ public class Widget1 extends AppWidgetProvider {
      * @param weatherBean 天气数据
      */
     private void updateAppWidget(Context context, int status, String district, WeatherBean weatherBean, int id) {
-//        ComponentName componentName = new ComponentName(context, this.getClass());
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget1);
 
-        if (status == NO_LOCATION) {
-            // 无定位
-//            noLocation(context, remoteViews, district, weatherBean);
-            remoteViews.setTextViewText(R.id.updateTime, "定位错误");
-        } else if (status == UPDATING) {
-            // 正在更新
-//            updating(context, remoteViews, district, weatherBean);
-        } else if (status == UPDATE_SUCCESS) {
-            // 更新成功
-//            updateSuccess(context, remoteViews, district, weatherBean);
+        if (status == UPDATE_SUCCESS) {
             showAppWidgetData(context, remoteViews, district, weatherBean);
-        } else if (status == UPDATE_FAILED) {
-            // 正在失败
-//            updateFail(context, remoteViews, district, weatherBean);
+        } else {
+            remoteViews.setTextViewText(R.id.updateTime, "定位错误");
         }
 
         AppWidgetManager.getInstance(context).updateAppWidget(id, remoteViews);
     }
-
-    /**
-     * 获取 RemoteViews 布局 id
-     */
-//    abstract protected int layoutId();
-//
-//    abstract protected void noLocation(Context context, RemoteViews remoteViews, String district, WeatherBean weatherBean);
-//
-//    abstract protected void updating(Context context, RemoteViews remoteViews, String district, WeatherBean weatherBean);
-//
-    protected void updateSuccess(Context context, RemoteViews remoteViews, String district, WeatherBean weatherBean) {
-//        // 打开APP首页的Intent
-//        PendingIntent launchPendingIntent = createLaunchPendingIntent(context);
-//        // 刷新的Intent
-//        PendingIntent updatePendingIntent = createUpdatePendingIntent(context);
-
-        showAppWidgetData(context, remoteViews, district, weatherBean);
-    }
-//    abstract protected void updateFail(Context context, RemoteViews remoteViews, String district, WeatherBean weatherBean);
-//
 
 
     private void showAppWidgetData(Context context, RemoteViews remoteViews, String district, WeatherBean weatherBean) {
